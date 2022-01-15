50a96faf1c92f4558069ba7bf20bcbb2dd4b4c9e
Krtucho <Krtucho98@gmail.com>
2022-01-15 15:32:10 -0500

    test_tiny_webserver

--- webserver.c
+++ webserver.c
@@ -12,9 +12,13 @@
 #include<fcntl.h>
 #include<sys/sendfile.h>
 #include<errno.h>
+#include <sys/mman.h>
 // #include <sys/types.h>
 //#include <sys/socket.h>
 
+#define MAXLINE 20000
+#define MAXBUF 20000
+#define LISTENQ 1024
 
 typedef struct {
     int rio_fd;/* Descriptor for this internal buf */
@@ -23,6 +27,8 @@ typedef struct {
     char rio_buf[RIO_BUFSIZE];/* Internal buffer */
     } rio_t;
 
+typedef struct sockaddr SA;
+
 void rio_readinitb(rio_t *rp, int fd)
 {
     rp->rio_fd = fd;
@@ -30,33 +36,33 @@ void rio_readinitb(rio_t *rp, int fd)
     rp->rio_bufptr = rp->rio_buf;
 }
 
-    static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n)
-    {
-        int cnt;
-        
-        while (rp->rio_cnt <= 0) {
-            /* Refill if buf is empty */
-            rp->rio_cnt = read(rp->rio_fd, rp->rio_buf,
-            sizeof(rp->rio_buf));
-            if (rp->rio_cnt < 0) {
-                if (errno != EINTR)/* Interrupted by sig handler return */
-                return -1;
-                }
-                else if (rp->rio_cnt == 0)/* EOF */
-                return 0;
-                else
-                rp->rio_bufptr = rp->rio_buf;/* Reset buffer ptr */
-                }
-                
-                /* Copy min(n, rp->rio_cnt) bytes from internal buf to user buf */
-                cnt=n;
-                if (rp->rio_cnt < n)
-                cnt = rp->rio_cnt;
-                memcpy(usrbuf, rp->rio_bufptr, cnt);
-                rp->rio_bufptr += cnt;
-                rp->rio_cnt -= cnt;
-                return cnt;
-                }
+static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n)
+{
+    int cnt;
+    
+    while (rp->rio_cnt <= 0) {
+        /* Refill if buf is empty */
+        rp->rio_cnt = read(rp->rio_fd, rp->rio_buf,
+        sizeof(rp->rio_buf));
+        if (rp->rio_cnt < 0) {
+            if (errno != EINTR)/* Interrupted by sig handler return */
+            return -1;
+            }
+            else if (rp->rio_cnt == 0)/* EOF */
+            return 0;
+            else
+            rp->rio_bufptr = rp->rio_buf;/* Reset buffer ptr */
+            }
+            
+            /* Copy min(n, rp->rio_cnt) bytes from internal buf to user buf */
+            cnt=n;
+            if (rp->rio_cnt < n)
+            cnt = rp->rio_cnt;
+            memcpy(usrbuf, rp->rio_bufptr, cnt);
+            rp->rio_bufptr += cnt;
+            rp->rio_cnt -= cnt;
+            return cnt;
+            }
 
 
 
@@ -106,6 +112,24 @@ ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n)
             return (n - nleft);/* Return >= 0 */
 }
 
+ssize_t rio_writen(int fd, void *usrbuf, size_t n)
+ { 
+    size_t nleft = n;
+ ssize_t nwritten;
+ char *bufp = usrbuf;
+while (nleft > 0) {
+ if ((nwritten = write(fd, bufp, nleft)) <= 0) {
+ if (errno == EINTR) /* Interrupted by sig handler return */
+ nwritten = 0; /* and call write() again */
+ else
+ return -1; /* errno set by write() */
+ }
+ nleft -= nwritten;
+ bufp += nwritten;
+ }
+ return n;
+}
+
 
 int open_clientfd(char *hostname, int port)
 {
@@ -126,89 +150,40 @@ int open_clientfd(char *hostname, int port)
     serveraddr.sin_port = htons(port);
     
     /* Establish a connection with the server */
-    if (connect(clientfd, (SA *) &serveraddr, sizeof(serveraddr)) < 0)
-    return -1;
-    return clientfd;
-    }
-
-
-    int open_listenfd(int port)
-    {
-        int listenfd, optval=1;
-        
-        struct sockaddr_in serveraddr;
-        
-        /* Create a socket descriptor */
-        if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if (connect(clientfd, (SA*) &serveraddr, sizeof(serveraddr)) < 0)
         return -1;
-        
-        /* Eliminates "Address already in use" error from bind */
-        if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
+    return clientfd;
+}
+
+
+int open_listenfd(int port)
+{
+    int listenfd, optval=1;
+    
+    struct sockaddr_in serveraddr;
+    
+    /* Create a socket descriptor */
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+        return -1;
+    
+    /* Eliminates "Address already in use" error from bind */
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,
         (const void *)&optval , sizeof(int)) < 0)
         return -1;
-        
-        /* Listenfd will be an end point for all requests to port16on any IP address for this host */
-        bzero((char *) &serveraddr, sizeof(serveraddr));
-        serveraddr.sin_family = AF_INET;
-        serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
-        serveraddr.sin_port = htons((unsigned short)port);
-        if (bind(listenfd, (SA *)&serveraddr, sizeof(serveraddr)) < 0)
+    
+    /* Listenfd will be an end point for all requests to port16on any IP address for this host */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serveraddr.sin_port = htons((unsigned short)port);
+    if (bind(listenfd, (SA *)&serveraddr, sizeof(serveraddr)) < 0)
         return -1;
-        
-        /* Make it a listening socket ready to accept connection requests */
-        if (listen(listenfd, LISTENQ) < 0)
+    
+    /* Make it a listening socket ready to accept connection requests */
+    if (listen(listenfd, LISTENQ) < 0)
         return -1;
-        return listenfd;
-        }
-
-
-        void doit(int fd)
-        {
-            int is_static;
-            struct stat sbuf;
-            char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-            char filename[MAXLINE], cgiargs[MAXLINE];
-            rio_t rio;
-            
-            /* Read request line and headers */
-            Rio_readinitb(&rio, fd);
-            Rio_readlineb(&rio, buf, MAXLINE);
-            sscanf(buf, "%s %s %s", method, uri, version);
-            if (strcasecmp(method, "GET")) {
-                clienterror(fd, method, "501", "Not Implemented",
-                "Tiny does not implement this method");
-                return;
-                }
-                read_requesthdrs(&rio);
-                
-                /* Parse URI from GET request */
-                is_static = parse_uri(uri, filename, cgiargs);
-                if (stat(filename, &sbuf) < 0) {
-                    clienterror(fd, filename, "404", "Not found",
-                    "Tiny couldn’t find this file");
-                    return;
-                    }
-                    
-                    if (is_static) {/* Serve static content */
-                    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
-                        clienterror(fd, filename, "403", "Forbidden",
-                        "Tiny couldn’t read the file");
-                        return;
-                        }
-                        serve_static(fd, filename, sbuf.st_size);
-                        }
-                        else {/* Serve dynamic content */
-                        if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
-                            clienterror(fd, filename, "403", "Forbidden",
-                            
-                            "Tiny couldn’t run the CGI program");
-                            return;
-                            }
-                            serve_dynamic(fd, filename, cgiargs);
-                            }
-                            }
-
-
+    return listenfd;
+}
 
 int parse_uri(char *uri, char *filename, char *cgiargs)
 {
@@ -218,7 +193,7 @@ int parse_uri(char *uri, char *filename, char *cgiargs)
     strcpy(cgiargs, "");
     strcpy(filename, ".");
     strcat(filename, uri);
-    if (uri[strlen(uri)-1] == ’/’)
+    if (uri[strlen(uri)-1] == '/')
     strcat(filename, "home.html");
     return 1;
     }
@@ -234,8 +209,20 @@ int parse_uri(char *uri, char *filename, char *cgiargs)
         strcat(filename, uri);
         return 0;
         }
-        }
+    }
 
+/*23* get_filetype - derive file type from file name24*/
+void get_filetype(char *filename, char *filetype)
+{
+    if (strstr(filename, ".html"))
+    strcpy(filetype, "text/html");
+    else if (strstr(filename, ".gif"))
+    strcpy(filetype, "image/gif");
+    else if (strstr(filename, ".jpg"))
+    strcpy(filetype, "image/jpeg");
+    else
+    strcpy(filetype, "text/plain");
+}
 
 void serve_static(int fd, char *filename, int filesize)
 {
@@ -248,30 +235,96 @@ void serve_static(int fd, char *filename, int filesize)
     sprintf(buf, "%sServer: Tiny Web Server\r\n", buf);
     sprintf(buf, "%sContent-length: %d\r\n", buf, filesize);
     sprintf(buf, "%sContent-type: %s\r\n\r\n", buf, filetype);
-    Rio_writen(fd, buf, strlen(buf));
+    rio_writen(fd, buf, strlen(buf));
     
     /* Send response body to client */
     
-    srcfd = Open(filename, O_RDONLY, 0);
-    srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
-    Close(srcfd);
-    Rio_writen(fd, srcp, filesize);
-    Munmap(srcp, filesize);
-    }
+    srcfd = open(filename, O_RDONLY, 0);
+    srcp = mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
+    close(srcfd);
+    rio_writen(fd, srcp, filesize);
+    munmap(srcp, filesize);
+}
+
+void serve_dynamic(int fd, char *filename, char *cgiargs)
+ { 
+      char buf[MAXLINE], *emptylist[] = { NULL };
+ /* Return first part of HTTP response */
+ sprintf(buf, "HTTP/1.0 200 OK\r\n");
+Rio_writen(fd, buf, strlen(buf));
+ sprintf(buf, "Server: Tiny Web Server\r\n");
+ Rio_writen(fd, buf, strlen(buf));
+
+ if (Fork() == 0) { /* child */
+ /* Real server would set all CGI vars here */
+ setenv("QUERY_STRING", cgiargs, 1);
+ Dup2(fd, STDOUT_FILENO); /* Redirect stdout to client */
+Execve(filename, emptylist, environ); /* Run CGI program */
+ }
+ Wait(NULL); /* Parent waits for and reaps child */
+}
     
-    /*23* get_filetype - derive file type from file name24*/
-    void get_filetype(char *filename, char *filetype)
-    {
-        if (strstr(filename, ".html"))
-        strcpy(filetype, "text/html");
-        else if (strstr(filename, ".gif"))
-        strcpy(filetype, "image/gif");
-        else if (strstr(filename, ".jpg"))
-        strcpy(filetype, "image/jpeg");
-        else
-        strcpy(filetype, "text/plain");
-        
+ void read_requesthdrs(rio_t *rp)
+ {  
+    char buf[MAXLINE];
+    rio_readlineb(rp, buf, MAXLINE);
+    while(strcmp(buf, "\r\n")) {
+        rio_readlineb(rp, buf, MAXLINE);
+        printf("%s", buf);
+    }
+    return;
+}
+    
+
+void doit(int fd)
+{
+    int is_static;
+    struct stat sbuf;
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char filename[MAXLINE], cgiargs[MAXLINE];
+    rio_t rio;
+    
+    /* Read request line and headers */
+    rio_readinitb(&rio, fd);
+    rio_readlineb(&rio, buf, MAXLINE);
+    sscanf(buf, "%s %s %s", method, uri, version);
+    if (strcasecmp(method, "GET")) {
+        clienterror(fd, method, "501", "Not Implemented",
+        "Tiny does not implement this method");
+        return;
         }
+        read_requesthdrs(&rio);
+        
+        /* Parse URI from GET request */
+        is_static = parse_uri(uri, filename, cgiargs);
+        if (stat(filename, &sbuf) < 0) {
+            clienterror(fd, filename, "404", "Not found",
+            "Tiny couldn’t find this file");
+            return;
+            }
+            
+        if (is_static) {/* Serve static content */
+        if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode)) {
+            clienterror(fd, filename, "403", "Forbidden",
+            "Tiny couldn’t read the file");
+            return;
+            }
+            serve_static(fd, filename, sbuf.st_size);
+            }
+            else {/* Serve dynamic content */
+            if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode)) {
+                clienterror(fd, filename, "403", "Forbidden",
+                
+                "Tiny couldn’t run the CGI program");
+                return;
+                }
+                serve_dynamic(fd, filename, cgiargs);
+                }
+            }
+
+
+
+
 
 
 
